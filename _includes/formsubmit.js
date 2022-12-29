e41197329b8507be41fe11bca9596f202ce0dbb4
Shantam Raj <shantam.raj@uzh.ch>
2022-12-29 14:55:08 +0100

    js seems to work locally - testing on server

---
+++ _includes/formsubmit.js
@@ -0,0 +1,43 @@
+function formSubmit(e){
+    e.preventDefault(); //This will prevent the default click action
+
+    // var frm = $('#'+ $(this).data('name') +'');
+    var frm = document.getElementById("formforserver");
+    const formObj = new FormData(frm);
+    const formData = JSON.stringify(Object.fromEntries(formObj.entries()))
+    console.log(JSON.stringify(Object.fromEntries(formObj.entries())));
+    // console.log(JSON.stringify(formData));
+
+
+// const data = { username: 'example' };
+
+fetch('http://localhost:8080/submit', {
+  method: 'POST', // or 'PUT'
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: formData,
+//   body: frm.serialize(),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+
+    // $.ajax({
+    //     type: frm.attr('method'),
+    //     url: 'http://aifutestgroup.eastus.azurecontainer.io:8000/submit',
+    //     data: frm.serialize(),
+    //     success: function (data) {
+    //         console.log('Submission was successful.');
+    //         console.log(data);
+    //     },
+    //     error: function (data) {
+    //         console.log('An error occurred.');
+    //         console.log(data);
+    //     }
+    // });
+}

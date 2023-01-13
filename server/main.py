a604000ca5ad1bf410e00d406a985119b637c395
Shantam Raj <shantam.raj@uzh.ch>
2023-01-13 18:06:50 +0100

    checking if sending back headers helps with CORS

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
 from app.db_utilities import get_records_from_table, submit
@@ -12,10 +13,15 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=False,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST"],
     allow_headers=["*"],
     expose_headers=["*"]
 )
+
+headers = {
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+    }
 
 class Data(BaseModel):
     article_title: str
@@ -30,13 +36,13 @@ async def heartbeat():
 @app.get("/records")
 async def get_records_from(table_name):
     records = get_records_from_table(table_name)
-    return records
+    return JSONResponse(content=records, headers=headers)
 
 # push reocrds to given table
 @app.post("/submit")
 async def post_new_record(data: Data):
     print(data)
-    return data
+    return JSONResponse(content=data, headers=headers)
     # return submit(data)
 
 if __name__=="__main__":

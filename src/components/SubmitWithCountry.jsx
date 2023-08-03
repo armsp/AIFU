de3311c12edbb5e5cf7a8f5e90e05ec23f88964e
armsp <shantam.raj@uzh.ch>
2023-08-03 21:50:06 +0200

    adding global

--- src/components/SubmitWithCountry.jsx
+++ src/components/SubmitWithCountry.jsx
@@ -45,6 +45,7 @@ function CountrySelect(props) {
 
 // From https://bitbucket.org/atlassian/atlaskit-mk-2/raw/4ad0e56649c3e6c973e226b7efaeb28cb240ccb0/packages/core/select/src/data/countries.js
 const countries = [
+  { code: 'global', label: "Global", phone: "NA"},
   { code: 'AD', label: 'Andorra', phone: '376' },
   {
     code: 'AE',

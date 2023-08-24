0872726879be9cf6df7f262e79bca11fdf7f8f2e
armsp <shantam.raj@uzh.ch>
2023-08-24 14:05:57 +0200

    fixing country table formdata

--- src/components/SubmitWithCountry.jsx
+++ src/components/SubmitWithCountry.jsx
@@ -9,15 +9,18 @@ import CustomSnackbar from './CustomSnackbar';
 import InfoIcon from '@mui/icons-material/Info';
 
 function CountrySelect(props) {
+  const { onSelect, ...otherProps } = props;
   return (
     <Autocomplete
       id="country-select"
       sx={{ width: 300 }}
       options={countries}
       autoHighlight
+      onChange={(event, option) => onSelect(option.alpha3 ? option.alpha3 : option.label)}
+      // getSelected={(option) => option.alpha3 ? option.alpha3 : option.label} // Compare based on alpha3 code
       getOptionLabel={(option) => option.label}
-      renderOption={(props, option) => (
-        <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
+      renderOption={(otherProps, option) => (
+        <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...otherProps}>
           <img
             loading="lazy"
             width="20"
@@ -31,7 +34,7 @@ function CountrySelect(props) {
       renderInput={(params) => (
         <TextField
           {...params}
-          {...props}
+          {...otherProps}
           label="Choose a country"
           inputProps={{
             ...params.inputProps,
@@ -45,32 +48,30 @@ function CountrySelect(props) {
 
 const countries = [
   { code: 'global', alpha3: 'global', label: "Global", phone: "NA"},
-  { code: 'AD', label: 'Andorra', phone: '376' },
+  { code: 'AD', alpha3: 'and', label: 'Andorra', phone: '376' },
   {
-    code: 'AE',
+    code: 'AE', alpha3: 'are',
     label: 'United Arab Emirates',
     phone: '971',
   },
-  { code: 'AF', label: 'Afghanistan', phone: '93' },
+  { code: 'AF', alpha3: 'afg', label: 'Afghanistan', phone: '93' },
   {
-    code: 'AG',
+    code: 'AG', alpha3: 'atg',
     label: 'Antigua and Barbuda',
     phone: '1-268',
   },
-  { code: 'AI', label: 'Anguilla', phone: '1-264' },
+  { code: 'AI', alpha3: 'aia', label: 'Anguilla', phone: '1-264' },
   { code: 'AL', label: 'Albania', phone: '355' },
   { code: 'AM', label: 'Armenia', phone: '374' },
   { code: 'AO', label: 'Angola', phone: '244' },
   { code: 'AQ', label: 'Antarctica', phone: '672' },
-  { code: 'AR', label: 'Argentina', phone: '54' },
+  { code: 'AR', alpha3: 'arg', label: 'Argentina', phone: '54' },
   { code: 'AS', label: 'American Samoa', phone: '1-684' },
-  { code: 'AT', label: 'Austria', phone: '43' },
+  { code: 'AT', alpha3: 'aut', label: 'Austria', phone: '43' },
   {
-    code: 'AU',
+    code: 'AU', alpha3: 'aus',
     label: 'Australia',
-    phone: '61',
-    suggested: true,
-  },
+    phone: '61'  },
   { code: 'AW', label: 'Aruba', phone: '297' },
   { code: 'AX', label: 'Alland Islands', phone: '358' },
   { code: 'AZ', label: 'Azerbaijan', phone: '994' },
@@ -80,8 +81,8 @@ const countries = [
     phone: '387',
   },
   { code: 'BB', label: 'Barbados', phone: '1-246' },
-  { code: 'BD', label: 'Bangladesh', phone: '880' },
-  { code: 'BE', label: 'Belgium', phone: '32' },
+  { code: 'BD', alpha3: 'bgd',label: 'Bangladesh', phone: '880' },
+  { code: 'BE', alpha3: 'bel',label: 'Belgium', phone: '32' },
   { code: 'BF', label: 'Burkina Faso', phone: '226' },
   { code: 'BG', label: 'Bulgaria', phone: '359' },
   { code: 'BH', label: 'Bahrain', phone: '973' },
@@ -91,7 +92,7 @@ const countries = [
   { code: 'BM', label: 'Bermuda', phone: '1-441' },
   { code: 'BN', label: 'Brunei Darussalam', phone: '673' },
   { code: 'BO', label: 'Bolivia', phone: '591' },
-  { code: 'BR', label: 'Brazil', phone: '55' },
+  { code: 'BR', alpha3: 'bra',label: 'Brazil', phone: '55' },
   { code: 'BS', label: 'Bahamas', phone: '1-242' },
   { code: 'BT', label: 'Bhutan', phone: '975' },
   { code: 'BV', label: 'Bouvet Island', phone: '47' },
@@ -99,7 +100,7 @@ const countries = [
   { code: 'BY', label: 'Belarus', phone: '375' },
   { code: 'BZ', label: 'Belize', phone: '501' },
   {
-    code: 'CA',
+    code: 'CA',alpha3: 'can',
     label: 'Canada',
     phone: '1',
     suggested: true,
@@ -124,12 +125,12 @@ const countries = [
     label: 'Congo, Republic of the',
     phone: '242',
   },
-  { code: 'CH', label: 'Switzerland', phone: '41' },
+  { code: 'CH', alpha3: 'che',label: 'Switzerland', phone: '41' },
   { code: 'CI', label: "Cote d'Ivoire", phone: '225' },
   { code: 'CK', label: 'Cook Islands', phone: '682' },
   { code: 'CL', label: 'Chile', phone: '56' },
   { code: 'CM', label: 'Cameroon', phone: '237' },
-  { code: 'CN', label: 'China', phone: '86' },
+  { code: 'CN',alpha3: 'chn', label: 'China', phone: '86' },
   { code: 'CO', label: 'Colombia', phone: '57' },
   { code: 'CR', label: 'Costa Rica', phone: '506' },
   { code: 'CU', label: 'Cuba', phone: '53' },
@@ -137,15 +138,15 @@ const countries = [
   { code: 'CW', label: 'Curacao', phone: '599' },
   { code: 'CX', label: 'Christmas Island', phone: '61' },
   { code: 'CY', label: 'Cyprus', phone: '357' },
-  { code: 'CZ', label: 'Czech Republic', phone: '420' },
+  { code: 'CZ',alpha3: 'cze', label: 'Czech Republic', phone: '420' },
   {
-    code: 'DE',
+    code: 'DE',alpha3: 'deu',
     label: 'Germany',
     phone: '49',
     suggested: true,
   },
   { code: 'DJ', label: 'Djibouti', phone: '253' },
-  { code: 'DK', label: 'Denmark', phone: '45' },
+  { code: 'DK',alpha3: 'dnk', label: 'Denmark', phone: '45' },
   { code: 'DM', label: 'Dominica', phone: '1-767' },
   {
     code: 'DO',
@@ -155,12 +156,12 @@ const countries = [
   { code: 'DZ', label: 'Algeria', phone: '213' },
   { code: 'EC', label: 'Ecuador', phone: '593' },
   { code: 'EE', label: 'Estonia', phone: '372' },
-  { code: 'EG', label: 'Egypt', phone: '20' },
+  { code: 'EG', alpha3: 'egy',label: 'Egypt', phone: '20' },
   { code: 'EH', label: 'Western Sahara', phone: '212' },
   { code: 'ER', label: 'Eritrea', phone: '291' },
-  { code: 'ES', label: 'Spain', phone: '34' },
+  { code: 'ES',alpha3: 'esp', label: 'Spain', phone: '34' },
   { code: 'ET', label: 'Ethiopia', phone: '251' },
-  { code: 'FI', label: 'Finland', phone: '358' },
+  { code: 'FI',alpha3: 'fin', label: 'Finland', phone: '358' },
   { code: 'FJ', label: 'Fiji', phone: '679' },
   {
     code: 'FK',
@@ -174,25 +175,25 @@ const countries = [
   },
   { code: 'FO', label: 'Faroe Islands', phone: '298' },
   {
-    code: 'FR',
+    code: 'FR',alpha3: 'fra',
     label: 'France',
     phone: '33',
     suggested: true,
   },
   { code: 'GA', label: 'Gabon', phone: '241' },
-  { code: 'GB', label: 'United Kingdom', phone: '44' },
+  { code: 'GB', alpha3: 'gbr',label: 'United Kingdom', phone: '44' },
   { code: 'GD', label: 'Grenada', phone: '1-473' },
   { code: 'GE', label: 'Georgia', phone: '995' },
   { code: 'GF', label: 'French Guiana', phone: '594' },
   { code: 'GG', label: 'Guernsey', phone: '44' },
   { code: 'GH', label: 'Ghana', phone: '233' },
   { code: 'GI', label: 'Gibraltar', phone: '350' },
-  { code: 'GL', label: 'Greenland', phone: '299' },
+  { code: 'GL',alpha3: 'grl', label: 'Greenland', phone: '299' },
   { code: 'GM', label: 'Gambia', phone: '220' },
   { code: 'GN', label: 'Guinea', phone: '224' },
   { code: 'GP', label: 'Guadeloupe', phone: '590' },
   { code: 'GQ', label: 'Equatorial Guinea', phone: '240' },
-  { code: 'GR', label: 'Greece', phone: '30' },
+  { code: 'GR',alpha3: 'grc', label: 'Greece', phone: '30' },
   {
     code: 'GS',
     label: 'South Georgia and the South Sandwich Islands',
@@ -202,7 +203,7 @@ const countries = [
   { code: 'GU', label: 'Guam', phone: '1-671' },
   { code: 'GW', label: 'Guinea-Bissau', phone: '245' },
   { code: 'GY', label: 'Guyana', phone: '592' },
-  { code: 'HK', label: 'Hong Kong', phone: '852' },
+  { code: 'HK', alpha3: 'hkg',label: 'Hong Kong', phone: '852' },
   {
     code: 'HM',
     label: 'Heard Island and McDonald Islands',
@@ -212,29 +213,29 @@ const countries = [
   { code: 'HR', label: 'Croatia', phone: '385' },
   { code: 'HT', label: 'Haiti', phone: '509' },
   { code: 'HU', label: 'Hungary', phone: '36' },
-  { code: 'ID', label: 'Indonesia', phone: '62' },
+  { code: 'ID', alpha3: 'idn',label: 'Indonesia', phone: '62' },
   { code: 'IE', label: 'Ireland', phone: '353' },
   { code: 'IL', label: 'Israel', phone: '972' },
   { code: 'IM', label: 'Isle of Man', phone: '44' },
-  { code: 'IN', label: 'India', phone: '91' },
+  { code: 'IN', alpha3: 'ind',label: 'India', phone: '91' },
   {
     code: 'IO',
     label: 'British Indian Ocean Territory',
     phone: '246',
   },
-  { code: 'IQ', label: 'Iraq', phone: '964' },
+  { code: 'IQ', alpha3: 'irq',label: 'Iraq', phone: '964' },
   {
-    code: 'IR',
+    code: 'IR',alpha3: 'irn',
     label: 'Iran, Islamic Republic of',
     phone: '98',
   },
   { code: 'IS', label: 'Iceland', phone: '354' },
-  { code: 'IT', label: 'Italy', phone: '39' },
+  { code: 'IT',alpha3: 'ita', label: 'Italy', phone: '39' },
   { code: 'JE', label: 'Jersey', phone: '44' },
   { code: 'JM', label: 'Jamaica', phone: '1-876' },
   { code: 'JO', label: 'Jordan', phone: '962' },
   {
-    code: 'JP',
+    code: 'JP',alpha3: 'jpn',
     label: 'Japan',
     phone: '81',
     suggested: true,
@@ -250,11 +251,11 @@ const countries = [
     phone: '1-869',
   },
   {
-    code: 'KP',
+    code: 'KP', alpha3: 'prk',
     label: "Korea, Democratic People's Republic of",
     phone: '850',
   },
-  { code: 'KR', label: 'Korea, Republic of', phone: '82' },
+  { code: 'KR', alpha3: 'kor',label: 'Korea, Republic of', phone: '82' },
   { code: 'KW', label: 'Kuwait', phone: '965' },
   { code: 'KY', label: 'Cayman Islands', phone: '1-345' },
   { code: 'KZ', label: 'Kazakhstan', phone: '7' },
@@ -263,10 +264,10 @@ const countries = [
     label: "Lao People's Democratic Republic",
     phone: '856',
   },
-  { code: 'LB', label: 'Lebanon', phone: '961' },
+  { code: 'LB', alpha3: 'lbn',label: 'Lebanon', phone: '961' },
   { code: 'LC', label: 'Saint Lucia', phone: '1-758' },
   { code: 'LI', label: 'Liechtenstein', phone: '423' },
-  { code: 'LK', label: 'Sri Lanka', phone: '94' },
+  { code: 'LK',alpha3: 'lka', label: 'Sri Lanka', phone: '94' },
   { code: 'LR', label: 'Liberia', phone: '231' },
   { code: 'LS', label: 'Lesotho', phone: '266' },
   { code: 'LT', label: 'Lithuania', phone: '370' },
@@ -294,7 +295,7 @@ const countries = [
     phone: '389',
   },
   { code: 'ML', label: 'Mali', phone: '223' },
-  { code: 'MM', label: 'Myanmar', phone: '95' },
+  { code: 'MM', alpha3: 'mmr',label: 'Myanmar', phone: '95' },
   { code: 'MN', label: 'Mongolia', phone: '976' },
   { code: 'MO', label: 'Macao', phone: '853' },
   {
@@ -309,8 +310,8 @@ const countries = [
   { code: 'MU', label: 'Mauritius', phone: '230' },
   { code: 'MV', label: 'Maldives', phone: '960' },
   { code: 'MW', label: 'Malawi', phone: '265' },
-  { code: 'MX', label: 'Mexico', phone: '52' },
-  { code: 'MY', label: 'Malaysia', phone: '60' },
+  { code: 'MX',alpha3: 'mex', label: 'Mexico', phone: '52' },
+  { code: 'MY', alpha3: 'mys',label: 'Malaysia', phone: '60' },
   { code: 'MZ', label: 'Mozambique', phone: '258' },
   { code: 'NA', label: 'Namibia', phone: '264' },
   { code: 'NC', label: 'New Caledonia', phone: '687' },
@@ -318,20 +319,20 @@ const countries = [
   { code: 'NF', label: 'Norfolk Island', phone: '672' },
   { code: 'NG', label: 'Nigeria', phone: '234' },
   { code: 'NI', label: 'Nicaragua', phone: '505' },
-  { code: 'NL', label: 'Netherlands', phone: '31' },
-  { code: 'NO', label: 'Norway', phone: '47' },
-  { code: 'NP', label: 'Nepal', phone: '977' },
+  { code: 'NL',alpha3: 'nld', label: 'Netherlands', phone: '31' },
+  { code: 'NO',alpha3: 'nor', label: 'Norway', phone: '47' },
+  { code: 'NP',alpha3: 'npl', label: 'Nepal', phone: '977' },
   { code: 'NR', label: 'Nauru', phone: '674' },
   { code: 'NU', label: 'Niue', phone: '683' },
-  { code: 'NZ', label: 'New Zealand', phone: '64' },
+  { code: 'NZ',alpha3: 'nzl', label: 'New Zealand', phone: '64' },
   { code: 'OM', label: 'Oman', phone: '968' },
   { code: 'PA', label: 'Panama', phone: '507' },
   { code: 'PE', label: 'Peru', phone: '51' },
   { code: 'PF', label: 'French Polynesia', phone: '689' },
   { code: 'PG', label: 'Papua New Guinea', phone: '675' },
-  { code: 'PH', label: 'Philippines', phone: '63' },
-  { code: 'PK', label: 'Pakistan', phone: '92' },
-  { code: 'PL', label: 'Poland', phone: '48' },
+  { code: 'PH',alpha3: 'phl', label: 'Philippines', phone: '63' },
+  { code: 'PK',alpha3: 'pak', label: 'Pakistan', phone: '92' },
+  { code: 'PL',alpha3: 'pol', label: 'Poland', phone: '48' },
   {
     code: 'PM',
     label: 'Saint Pierre and Miquelon',
@@ -344,21 +345,21 @@ const countries = [
     label: 'Palestine, State of',
     phone: '970',
   },
-  { code: 'PT', label: 'Portugal', phone: '351' },
+  { code: 'PT',alpha3: 'prt', label: 'Portugal', phone: '351' },
   { code: 'PW', label: 'Palau', phone: '680' },
-  { code: 'PY', label: 'Paraguay', phone: '595' },
-  { code: 'QA', label: 'Qatar', phone: '974' },
+  { code: 'PY',alpha3: 'pry', label: 'Paraguay', phone: '595' },
+  { code: 'QA',alpha3: 'qat', label: 'Qatar', phone: '974' },
   { code: 'RE', label: 'Reunion', phone: '262' },
   { code: 'RO', label: 'Romania', phone: '40' },
   { code: 'RS', label: 'Serbia', phone: '381' },
-  { code: 'RU', label: 'Russian Federation', phone: '7' },
+  { code: 'RU',alpha3: 'rus', label: 'Russian Federation', phone: '7' },
   { code: 'RW', label: 'Rwanda', phone: '250' },
-  { code: 'SA', label: 'Saudi Arabia', phone: '966' },
+  { code: 'SA',alpha3: 'sau', label: 'Saudi Arabia', phone: '966' },
   { code: 'SB', label: 'Solomon Islands', phone: '677' },
   { code: 'SC', label: 'Seychelles', phone: '248' },
   { code: 'SD', label: 'Sudan', phone: '249' },
-  { code: 'SE', label: 'Sweden', phone: '46' },
-  { code: 'SG', label: 'Singapore', phone: '65' },
+  { code: 'SE',alpha3: 'swe', label: 'Sweden', phone: '46' },
+  { code: 'SG',alpha3: 'sgp', label: 'Singapore', phone: '65' },
   { code: 'SH', label: 'Saint Helena', phone: '290' },
   { code: 'SI', label: 'Slovenia', phone: '386' },
   {
@@ -402,14 +403,14 @@ const countries = [
     phone: '262',
   },
   { code: 'TG', label: 'Togo', phone: '228' },
-  { code: 'TH', label: 'Thailand', phone: '66' },
+  { code: 'TH',alpha3: 'tha', label: 'Thailand', phone: '66' },
   { code: 'TJ', label: 'Tajikistan', phone: '992' },
   { code: 'TK', label: 'Tokelau', phone: '690' },
   { code: 'TL', label: 'Timor-Leste', phone: '670' },
   { code: 'TM', label: 'Turkmenistan', phone: '993' },
   { code: 'TN', label: 'Tunisia', phone: '216' },
   { code: 'TO', label: 'Tonga', phone: '676' },
-  { code: 'TR', label: 'Turkey', phone: '90' },
+  { code: 'TR',alpha3: 'tur', label: 'Turkey', phone: '90' },
   {
     code: 'TT',
     label: 'Trinidad and Tobago',
@@ -417,7 +418,7 @@ const countries = [
   },
   { code: 'TV', label: 'Tuvalu', phone: '688' },
   {
-    code: 'TW',
+    code: 'TW',alpha3: 'twn',
     label: 'Taiwan, Republic of China',
     phone: '886',
   },
@@ -426,10 +427,10 @@ const countries = [
     label: 'United Republic of Tanzania',
     phone: '255',
   },
-  { code: 'UA', label: 'Ukraine', phone: '380' },
+  { code: 'UA',alpha3: 'ukr', label: 'Ukraine', phone: '380' },
   { code: 'UG', label: 'Uganda', phone: '256' },
   {
-    code: 'US',
+    code: 'US',alpha3: 'usa',
     label: 'United States',
     phone: '1',
     suggested: true,
@@ -464,7 +465,7 @@ const countries = [
   { code: 'XK', label: 'Kosovo', phone: '383' },
   { code: 'YE', label: 'Yemen', phone: '967' },
   { code: 'YT', label: 'Mayotte', phone: '262' },
-  { code: 'ZA', label: 'South Africa', phone: '27' },
+  { code: 'ZA', alpha3: 'zaf',label: 'South Africa', phone: '27' },
   { code: 'ZM', label: 'Zambia', phone: '260' },
   { code: 'ZW', label: 'Zimbabwe', phone: '263' },
 ];
@@ -473,6 +474,10 @@ const SubmissionFormWithCountry = () => {
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState('');
   const [snackbarSeverity, setSnackbarSeverity] = useState('');
+  const [selectedCountryAlpha3, setSelectedCountryAlpha3] = useState('');
+  const handleCountrySelect = (alpha3) => {
+    setSelectedCountryAlpha3(alpha3); // Update the selected country alpha3
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -480,8 +485,8 @@ const SubmissionFormWithCountry = () => {
   
     const formData = new FormData(e.target);
     const requestBody = Object.fromEntries(formData.entries());
-    // requestBody.table = tablename;
-    console.log(formData);
+    console.log(selectedCountryAlpha3);
+    requestBody.table = selectedCountryAlpha3;
     console.log(requestBody);
     try {
       const response = await fetch('https://aifuv2.eastus.azurecontainer.io/submit', {
@@ -577,7 +582,9 @@ const SubmissionFormWithCountry = () => {
                       />
                     </Grid>
                     <Grid item xs={12}>
-                        <CountrySelect required
+                        <CountrySelect 
+                        required
+                        onSelect={handleCountrySelect}
                         id="country"
                         name="table"
                         label="country"

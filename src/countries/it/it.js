6f8137c995382d68c0a70bbca68f7983ebb9744c
armsp <shantam.raj@uzh.ch>
2023-09-03 23:03:46 +0300

    adding italy

---
+++ src/countries/it/it.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import CountryData from '../../components/CountryCommon';
+
+export default function Ita(){
+    return(
+        <CountryData country="ita" />
+    )
+}
